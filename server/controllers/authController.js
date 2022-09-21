1b41c43e5490a6b0178c3ac4bc26dbadb184d2a5
And4Web <anands2807@gmail.com>
2022-09-21 21:50:36 +0530

    register route success

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,26 @@
+const User = require('../models/userModel');
+
 exports.signup = (req, res) => {
-  res.send("Hello this is the signup route.");
+  // res.json("Hello this is the signup route.");
+  const {name, email, password} = req.body;
+
+  User.findOne({email}).exec((err, user) => {
+    if(user){
+      return res.status(400).json({
+        error: 'This email already exists.'
+      })
+    }
+    let newUser = new User({name, email, password});
+    newUser.save((err, success) => {
+      if(err){
+        consol.log("SIGNUP ERROR: ", err);
+        return res.status(404).json({
+          error: err
+        });
+      }
+      res.json({
+        message: "Registered successfully."
+      })
+    })
+  })
 }
