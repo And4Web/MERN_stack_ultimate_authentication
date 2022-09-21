c4db6194954cef34112630cbd43278c3a495b9af
And4Web <anands2807@gmail.com>
2022-09-21 21:29:17 +0530

    validation applied on auth route.

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,9 +4,13 @@ const router = express.Router();
 //import controllers
 const {signup} = require('../controllers/authController');
 
+// import validators
+const {userSignupValidator} = require("../validations/authValidation");
+const{runValidation} = require('../validations');
+
 router.get('/test', (req, res) => {
   res.send("Hello this is the test route.");
 })
-router.get('/signup', signup);
+router.post('/signup',userSignupValidator, runValidation, signup);
 
 module.exports = router;
