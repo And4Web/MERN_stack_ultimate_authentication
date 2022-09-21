195b3e82b150814309cf5f25bc3d736a78969d5e
And4Web <anands2807@gmail.com>
2022-09-21 19:12:07 +0530

    implemented necessary middlewares

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
@@ -7,12 +12,22 @@ const port = process.env.PORT || 5000;
 //import routes
 const authRouter = require('./routes/authRoute');
 
-app.use(express.json());
+//app middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors); //cross origin requests
+if(process.env.NODE_ENV = 'development'){
+  app.use(cors({origin: 'http://localhost:3000'}))
+}
+
+//middleware
+
 
 app.get('/', (req, res) => {
   res.json("Hello Anand!");
 })
 
+//implement routes
 app.use('/api/auth', authRouter)
 
-app.listen(port, () => console.log(`Server is running at port: ${port}.`))
+app.listen(port, () => console.log(`Server is running at port: ${port} - ${process.env.NODE_ENV}`))
